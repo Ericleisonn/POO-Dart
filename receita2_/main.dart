ecfd3d229b742bcbb01f467a1b3d546f89013193
Ericleisonn <111910474+Ericleisonn@users.noreply.github.com>
2023-04-11 19:30:36 -0300

    Update main.dart (Tarefa 1 ok)

--- receita2_/main.dart
+++ receita2_/main.dart
@@ -1,1 +1,45 @@
+import 'package:flutter/material.dart';
 
+class BottomItens extends StatelessWidget {
+  BottomItens();
+  void botaoFoiTocado(int index) {
+    print("Tocaram no botão $index");}
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(onTap: botaoFoiTocado, items: const [
+      BottomNavigationBarItem(
+        label: "Cafés",
+        icon: Icon(Icons.coffee_outlined),),
+      BottomNavigationBarItem(
+          label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
+      BottomNavigationBarItem(label: "Nações", icon: Icon(Icons.flag_outlined))]);
+  }}
+
+
+class NewBody extends StatelessWidget {
+  NewBody();
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: const [
+          Expanded(
+            child: Text("La Fin Du Monde - Bock - 65 ibu"),
+          ),
+          Expanded(
+            child: Text("Sapporo Premiume - Sour Ale - 54 ibu"),
+          ),
+          Expanded(
+            child: Text("Duvel - Pilsner - 82 ibu"),
+          )
+        ]);
+  }}
+
+void main() {
+   MaterialApp app = MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+home: Scaffold(
+        appBar: AppBar(title: const Text("Dicas")),
+        body: (NewBody()),
+        bottomNavigationBar: BottomItens(),
+      ));
+  runApp(app);
+}
