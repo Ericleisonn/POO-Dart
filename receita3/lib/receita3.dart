0fde27d06294c28538e64e39a4ec9800a5ceb667
Ericleisonn <ericleisonrn@gmail.com>
2023-04-22 16:55:46 -0300

    Exercício 1 ok

--- receita3/lib/receita3.dart
+++ receita3/lib/receita3.dart
@@ -15,109 +15,56 @@ void main() {
 
 
 class MyApp extends StatelessWidget {
-
   @override
-
   Widget build(BuildContext context) {
-
     return MaterialApp(
-
-        theme: ThemeData(primarySwatch: Colors.deepPurple),
-
-        debugShowCheckedModeBanner: false,
-
-        home: Scaffold(
-
-          appBar: AppBar(
-
-            title: const Text("Dicas"),
-
-          ),
-
-          body: DataBodyWidget(),
-
-          bottomNavigationBar: NewNavBar(),
-
-        ));
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      debugShowCheckedModeBanner:false,
+      home: Scaffold(
+        appBar: AppBar( 
+          title: const Text("Dicas"),),
+        body: DataBodyWidget(objects:const [
+          "La Fin Du Monde - Bock - 65 ibu",
+          "Sapporo Premiume - Sour Ale - 54 ibu",
+          "Duvel - Pilsner - 82 ibu"
+        ]),
+        bottomNavigationBar: NewNavBar(
+          icons: const [
+            Icon(Icons.coffee_outlined),
+            Icon(Icons.local_drink_outlined),
+            Icon(Icons.flag_outlined)
+            ])
+      ));
 
   }
 
 }
-
-
 
 class NewNavBar extends StatelessWidget {
-
-  NewNavBar();
-
-
-
+  // NewNavBar();
+  late List<Icon> icons;
+  NewNavBar({this.icons = const []});
   void botaoFoiTocado(int index) {
-
     print("Tocaram no botão $index");
-
   }
-
-
-
   @override
-
-  Widget build(BuildContext context) {
-
-    return BottomNavigationBar(onTap: botaoFoiTocado, items: const [
-
-      BottomNavigationBarItem(
-
-        label: "Cafés",
-
-        icon: Icon(Icons.coffee_outlined),
-
-      ),
-
-      BottomNavigationBarItem(
-
-          label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
-
-      BottomNavigationBarItem(label: "Nações", icon: Icon(Icons.flag_outlined))
-
-    ]);
-
-  }
-
-}
-
+    Widget build(BuildContext context) {
+    return BottomNavigationBar(onTap: botaoFoiTocado, 
+      items: icons.map((icon) => BottomNavigationBarItem(label: "Botão",icon: icon)).toList());
+  }}
 
 
 class DataBodyWidget extends StatelessWidget {
-
-  DataBodyWidget();
-
-  @override
-
-  Widget build(BuildContext context) {
-
-    return Column(children: [
-
-      Expanded(
-
-        child: Text("La Fin Du Monde - Bock - 65 ibu"),
-
-      ),
-
-      Expanded(
-
-        child: Text("Sapporo Premiume - Sour Ale - 54 ibu"),
-
-      ),
-
-      Expanded(
-
-        child: Text("Duvel - Pilsner - 82 ibu"),
-
-      )
-
-    ]);
-
+  List<String> objects;
+  DataBodyWidget( {this.objects = const [] });
+  Expanded processarUmElemento(String obj){
+    return Expanded(                
+          child: Center(child: Text(obj)),
+        );
   }
-
-}
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: objects.map( 
+      (obj) => Expanded(
+        child: Center(child: Text(obj)))).toList());
+}}
