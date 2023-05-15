95621bd4ab1c6f15d54bb827f08fb62aabece3d7
Ericleisonn <ericleisonrn@gmail.com>
2023-05-15 15:41:42 -0300

    Atualização

--- receita5/lib/receita5.dart
+++ receita5/lib/receita5.dart
@@ -1,11 +1,46 @@
-// ignore_for_file: use_key_in_widget_constructors
-
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-var dataObjects = [];
 
-     // EXERCÍCIO 1 
-//  1. No print da classe HookWidget, as mudanças de estado de um botão para outra da bottomNavigationBar são contabilizadas no console, ou seja, em toda mudança de estado de qualquer classe, elas são contabilizadas e o ícone é redenrizado novamente.
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+
+final ValueNotifier<List> tableStateNotifier = new ValueNotifier([]);
+//var dataObjects = [];
+
+void carregarCervejas(){
+
+  tableStateNotifier.value = [{
+
+          "name": "La Fin Du Monde",
+
+          "style": "Bock",
+
+          "ibu": "65"
+
+          },
+
+          {
+
+          "name": "Sapporo Premiume",
+
+          "style": "Sour Ale",
+
+          "ibu": "54"
+
+          },
+
+          {
+
+          "name": "Duvel", 
+
+          "style": "Pilsner", 
+
+          "ibu": "82"
+
+          }
+
+        ];
+
+}
 
 void main() {
 
@@ -19,43 +54,160 @@ void main() {
 
 class MyApp extends StatelessWidget {
 
+
+
   @override
 
   Widget build(BuildContext context) {
-    print("no build da classe $MyApp");
-    
 
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      debugShowCheckedModeBanner:false,
-      home: Scaffold(
-        appBar: AppBar( 
-          title: const Text("Dicas"),
-          ),
-        body: DataTableWidget(jsonObjects:dataObjects),
-        bottomNavigationBar: NewNavBar2(),
-      ));}}
 
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+
+      debugShowCheckedModeBanner:false,
+
+      home: Scaffold(
+
+        appBar: AppBar( 
+
+          title: const Text("Dicas"),
+
+          ),
+
+        body: ValueListenableBuilder(
+
+          valueListenable: tableStateNotifier,
+
+          builder:(_, value, __){
+
+            return DataTableWidget(
+
+              jsonObjects: value, 
+
+              propertyNames: ["name","style","ibu"], 
+
+              columnNames: ["Nome", "Estilo", "IBU"]
+
+            );
+
+          }
+
+        ),
+
+        bottomNavigationBar: NewNavBar(),
+
+      ));
+
+  }
+
+
+
+}
+
+
+
+class NewNavBar extends HookWidget {
+
+  NewNavBar();
+
+
+
+  @override
+
+  Widget build(BuildContext context) {
+
+    var state = useState(1);
+
+    return BottomNavigationBar(
+
+      onTap: (index){
+
+        state.value = index;
+
+        tableStateNotifier.value = [{
+
+          "name": "La Fin Du Monde",
+
+          "style": "Bock",
+
+          "ibu": "65"
+
+          },
+
+          {
+
+          "name": "Sapporo Premiume",
+
+          "style": "Sour Ale",
+
+          "ibu": "54"
+
+          },
+
+          {
+
+          "name": "Duvel", 
+
+          "style": "Pilsner", 
+
+          "ibu": "82"
+
+          }
+
+        ];
+
+      }, 
+
+      currentIndex: state.value,
+
+      items: const [
+
+        BottomNavigationBarItem(
+
+          label: "Cafés",
+
+          icon: Icon(Icons.coffee_outlined),
+
+        ),
+
+
+
+        BottomNavigationBarItem(
+
+            label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
+
+
+
+        BottomNavigationBarItem(label: "Nações", icon: Icon(Icons.flag_outlined))
+
+      ]);
+
+
+
+  }
+
+
+
+}
 class DataTableWidget extends StatelessWidget {
+
+
 
   final List jsonObjects;
 
-  DataTableWidget( {this.jsonObjects = const [] });
+  final List<String> columnNames;
+
+  final List<String> propertyNames;
+
+
+
+  DataTableWidget( {this.jsonObjects = const [], this.columnNames = const ["Nome","Estilo","IBU"], this.propertyNames= const ["name", "style", "ibu"]});
 
 
 
   @override
 
   Widget build(BuildContext context) {
-    print("no build da classe $DataTableWidget");
-
-    
-
-    var columnNames = ["Nome","Estilo","IBU"],
-
-        propertyNames = ["name", "style", "ibu"];
-
-    
 
     return DataTable(
 
@@ -65,7 +217,7 @@ class DataTableWidget extends StatelessWidget {
 
                   label: Expanded(
 
-                    child: Text(name, style: const TextStyle(fontStyle: FontStyle.italic))
+                    child: Text(name, style: TextStyle(fontStyle: FontStyle.italic))
 
                   )
 
@@ -76,39 +228,21 @@ class DataTableWidget extends StatelessWidget {
       ,
 
       rows: jsonObjects.map( 
-        (obj) => DataRow(cells: propertyNames.map((propName) => DataCell(Text(obj[propName]))).toList())
-        ).toList());}}
+
+        (obj) => DataRow(
+
+            cells: propertyNames.map(
+
+              (propName) => DataCell(Text(obj[propName]))
+
+            ).toList()
+
+          )
+
+        ).toList());
+
+  }
 
 
-class NewNavBar2 extends StatefulWidget {
-  NewNavBar2();
-  @override
-  _ItemCounterState createState() => _ItemCounterState();
-  }
-  class _ItemCounterState extends State<NewNavBar2> {
-    _ItemCounterState();
-    int state = 0;
-    void buttonTapped(int index) {
-    print("Tocaram no botão $index");
-  }
-    @override
-  Widget build(BuildContext context) {
-    print("no build da classe NewNavBar2");
-      return BottomNavigationBar(
-        onTap: (index){
-          setState(() {
-            state = index;
-          });
-  },
-  currentIndex: state,
-  items: const [
-          BottomNavigationBarItem(
-            label: "Cafés",
-            icon: Icon(Icons.coffee_outlined),
-          ),
-          BottomNavigationBarItem(
-              label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
-          BottomNavigationBarItem(
-              label: "Nações", icon: Icon(Icons.flag_outlined))
-        ]);
-  }}
+
+}
